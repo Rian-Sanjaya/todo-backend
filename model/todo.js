5f33fc3225ab074ed658e468eae1d81c9186b3b7
Rian <rian.sj@gmail.com>
2019-02-22 11:21:09 +0700

    fill sample model

--- model/todo.js
+++ model/todo.js
@@ -1,4 +1,15 @@
 
-let todoModel = [];
+let todoModel = [
+    {
+        id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
+        todo: 'Todo 1',
+        description: 'Todo 1 description'
+    },
+    {
+        id: 'b3abd640-c92b-11e8-b02f-cbfa15db428b',
+        todo: 'Todo 2',
+        description: 'Todo 2 description'
+    }
+];
 
 module.exports = todoModel;
